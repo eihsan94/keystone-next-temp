e631226a4072512f1d5c1f562419aa159cae0124
Engku Ihsan <eihsan94@gmail.com>
2021-09-23 16:06:00 +0900

    change graphql playground

--- keystone.ts
+++ keystone.ts
@@ -10,13 +10,22 @@ const Post = list({
 });
 
 export default config({
-    db: {
-        provider: 'postgresql',
-        url: 'postgres://unusbvclcikzgs:4d7c53804f90b1bce55b71f9f5a9ceb612e3a08ebc971b792ceaffb71ce52cd8@ec2-23-20-208-173.compute-1.amazonaws.com:5432/d6k9m8l6hm053g',
-    },
+  db: {
+      provider: 'postgresql',
+      url: 'postgres://unusbvclcikzgs:4d7c53804f90b1bce55b71f9f5a9ceb612e3a08ebc971b792ceaffb71ce52cd8@ec2-23-20-208-173.compute-1.amazonaws.com:5432/d6k9m8l6hm053g',
+  },
   experimental: {
     generateNextGraphqlAPI: true,
     generateNodeAPI: true,
   },
   lists: { Post },
+  graphql: {
+    debug: true,
+    queryLimits: { maxTotalResults: 100 },
+    path: '/api/graphql',
+    apolloConfig: {
+      playground: true,
+      introspection: true,
+    },
+  },
 });
